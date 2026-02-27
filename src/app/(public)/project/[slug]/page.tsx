1c071425025bfb0356f1da1e4db6741757f77526
PleBea <op@plebea.com>
2026-02-27 23:15:13 +0900

    fix project detail lookup for published slugs

--- src/app/(public)/project/[slug]/page.tsx
+++ src/app/(public)/project/[slug]/page.tsx
@@ -10,7 +10,7 @@ export default async function ProjectDetailPage({
 }) {
   const { slug } = await params;
 
-  const project = await prisma.project.findUnique({
+  const project = await prisma.project.findFirst({
     where: { slug, isPublished: true },
     include: { club: true },
   });
